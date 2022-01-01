02659a0607510af073494f5c0d660048b7f1fcf2
WuggyX2 <matti.e.virkkunen@student.jyu.fi>
2022-01-01 19:12:26 +0200

    start implementing config file write

--- src/command/config.rs
+++ src/command/config.rs
@@ -1,6 +1,7 @@
-use dirs::home_dir;
+use serde::{Deserialize, Serialize};
 use std::fs;
-use std::path::Path;
+use std::fs::File;
+use std::io::prelude::*;
 use std::str::FromStr;
 use structopt::StructOpt;
 
@@ -21,18 +22,54 @@ impl Config {
     }
 
     fn write(name: &SettingFieldName, value: &str) {
-        let mut home_path = dirs::home_dir().unwrap();
+        let mut home_path = match dirs::home_dir() {
+            Some(folder_path) => folder_path,
+            None => {
+                println!("Could not find users home path");
+                return;
+            }
+        };
+
         home_path.push(".harvest/config");
         let config_folder = home_path.as_path();
 
         if config_folder.exists() {
-            println!("settings folder exists");
+            home_path.push("config.toml");
+            let config_file = home_path.as_path();
+
+            if config_file.exists() {
+                let mut file = File::open(config_file).expect("Config File not found");
+                let mut contents = String::new();
+                file.read_to_string(&mut contents);
+                let config_file: ConfigFile = toml::from_str(&contents).unwrap();
+                println!("{:?}", config_file);
+            } else {
+                let mut f = File::create(config_file).expect("Could no create file");
+
+                let config = ConfigFile {
+                    account_id: value.to_string(),
+                    access_token: None,
+                    user_agent: None,
+                };
+
+                let serialized = toml::to_string(&config).unwrap();
+
+                f.write_all(serialized.as_bytes());
+
+                f.sync_all();
+            }
         } else {
             fs::create_dir_all(config_folder).expect("Failed to create settings folder");
         }
     }
 
     fn printValue() {}
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct ConfigFile {
+    account_id: String,
+    access_token: Option<String>,
+    user_agent: Option<String>,
 }
 
 #[derive(Debug)]
